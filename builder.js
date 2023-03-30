abd2874a78000d600c4642cd4a4d75d02d768f3d
Bolarinwa Olakunle <joberror@gmail.com>
2023-03-30 15:42:50 +0100

    * 🐛 fix(builder.js): change deleteFile function to deleteFiles and delete multiple files * ✨ feat(builder.js): add functionality to output folder structure to a text file * ✨ feat(builder.js): add support for folder-structure.txt file * ✨ feat(builder.js): add support for saving data to a JSON file * ✨ feat(builder.js): add support for deleting multiple files * ✨ feat(builder.js): add support for zipping the svg folder The changes in this commit add several features to the builder.js file. The deleteFile function has been changed to deleteFiles to delete multiple files. The printFolderStructure function has been added to output the folder structure to a text file. The folder-structure.txt file has been added to support this feature. The functionality to save data to a JSON file has been added. The support for deleting multiple files has been added. The support for zipping the svg folder has been added
    * 🐛 fix(data.json): fix typo in icon names
    The commit fixes a typo in the icon names in the data.json file. The word "strike-through" was changed to "strikethrough" and "subtract" was changed to "substract" to match the correct icon names.
    
    * 🚚 chore(svg): rename strikethrough.svg to strike-through.svg
    * 🚚 chore(svg): rename substract.svg to subtract.svg
    The commit renames the strikethrough.svg file to strike-through.svg and substract.svg to subtract.svg. This improves the consistency of the naming convention used in the project.
    
    * 🆕 add(folder-structure.txt): add new SVG files to the project
    A new folder structure file has been added to the project. It contains a list of SVG files that have been added to the project in the arrows, browser, editor, and finance folders. These SVG files are used to display icons in the application.
    
    * 🔄 chore(refinedicons-all.zip): update icon set
    The refinedicons-all.zip file has been updated, likely with new or updated icons.

--- builder.js
+++ builder.js
@@ -6,58 +6,94 @@
 
 const fs = require("fs");
 const zip = require("adm-zip");
-const zipFile = new zip()
+const zipFile = new zip();
 
 // -------------- Runtime and processing -------------------
 
 // Set defaults
-let // main svg source
-source = "./public/svg/";
+const // main svg source
+	source = "./public/svg/";
 // data file
-let
-// main svg source
-data = "./content/data.json";
+const // main svg source
+	data = "./content/data.json";
 // store category name, icons and total
-let
-allData = [];
-let
-// svg zip file
-refined = `${source}refinedicons-all.zip`;
+const allData = [];
+const // svg zip file
+	refined = `${source}refinedicons-all.zip`;
+const // folder-structure
+	structure = `${source}folder-structure.txt`;
 
 // A function to delete a file
-async function deleteFile(file) {
-	fs.rmSync(file, {recursive: true, force: true})
+async function deleteFiles(files) {
+	for (const file of files) {
+		fs.rmSync(file, { recursive: true, force: true });
+	}
 }
 
-// Delete file and create a new generated zip file
-deleteFile(refined).then(() => {
-	// read svg folder, store category and icons in an array
-	fs.readdirSync(source).forEach((cat) => {
-		let tmp = {};
-		tmp.category = cat;
-		tmp.icons = fs
-			.readdirSync(source + cat)
-			.map((file) => file.split(".").slice(0, -1).join("."));
-		tmp.total = tmp.icons.length;
-		allData.push(tmp);
-	});
-}).then(() => {
-	// save the data captured above in a JSON file with a success and error callback
-	fs.writeFile(data, JSON.stringify(allData), "utf8", (err) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(`Saved data to ${data}`);
+// Delete files and create a new generated zip file
+deleteFiles([refined, structure])
+	.then(() => {
+		// read svg folder, store category and icons in an array
+		fs.readdirSync(source).forEach((cat) => {
+			const tmp = {};
+			tmp.category = cat;
+			tmp.icons = fs
+				.readdirSync(source + cat)
+				.map((file) => file.split(".").slice(0, -1).join("."));
+			tmp.total = tmp.icons.length;
+			allData.push(tmp);
+		});
+	})
+	.then(() => {
+		// save the data captured above in a JSON file with a success and error callback
+		fs.writeFile(data, JSON.stringify(allData), "utf8", (err) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(`Saved data to ${data}`);
+			}
+		});
+	})
+	.then(() => {
+		// Print and output svg folder structure to a text-file
+		function printFolderStructure(
+			path,
+			prefix = "",
+			isLast = true,
+			output = ""
+		) {
+			const files = fs.readdirSync(path);
+			const arrows = isLast ? "└── " : "├── ";
+			files.forEach((file, index) => {
+				const filePath = `${path}/${file}`;
+				const stats = fs.statSync(filePath);
+				const isDirectory = stats.isDirectory();
+				const symbolPrefix = isLast ? "    " : "│   ";
+				const fileName = isDirectory ? `${file}/` : file;
+				output += prefix + arrows + fileName + "\n";
+				if (isDirectory) {
+					const newPrefix = prefix + symbolPrefix;
+					const newLast = index === files.length - 1;
+					output = printFolderStructure(filePath, newPrefix, newLast, output);
+				}
+			});
+
+			return output;
 		}
+
+		const folderStructure = printFolderStructure(source);
+		fs.writeFileSync(structure, folderStructure);
+		console.log("Folder structure saved!");
+	})
+	.then(() => {
+		// zip the svg folder
+		zipFile.addLocalFolder(source);
+		zipFile.writeZip(refined);
+		console.log(`Zipped folder saved to ${refined}`);
+	})
+	.catch((error) => {
+		console.error("Error:", error);
 	});
-}).then(() => {
-	// zip the svg folder
-	zipFile.addLocalFolder(source);
-	zipFile.writeZip(refined);
-	console.log(`Zipped folder saved to ${refined}`);
-}).catch((error) => {
-	console.error('Error:', error)
-});
 
 // --------------- Debugging and test ----------------------
 
